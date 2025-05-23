56a3ac00233e660c1c09c9f07b755c6f5ec4b3c1
Darina Stoyanova-Mateva <darina.stoyanovamateva@gmail.com>
2025-05-23 16:41:55 +0000

    lambda codes

---
+++ basic-codes/create_lambda_code.py
@@ -0,0 +1,16 @@
+import json         
+import boto3            
+def lambda_handler(event, context):         
+    try:
+        sqsClient = boto3.client("sqs", region_name="us-east-1")        
+        try:        
+            response = sqsClient.receive_message(       
+            QueueUrl="<your Queue URL>",  #Please paste the SQS Queue URL
+            AttributeNames=['All'],
+            )
+            print(response)
+            return response
+        except Exception as e:
+            print("Get queue message failed because ", e)
+    except Exception as e:
+        print("Client connection to SQS failed because ", e)
